7450a0772df2fa3c3af2994104f9b7f286d73891
/ </>
2024-12-06 10:46:20 +0100

    fix reordering

--- src/artifacteditor/src/App.svelte
+++ src/artifacteditor/src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { ArtifactType, type IArtifact } from './lib/interfaces';
     import SvelteMarkdown from 'svelte-markdown';
+    import { Carta, MarkdownEditor } from 'carta-md';
 
     let artifacts: IArtifact[] = [
         {
@@ -83,12 +84,17 @@
             return;
         draggingIndex = parseInt(target.getAttribute('artifact-index')!);
         draggingPosition = event.y;
+
+        const root = traverseToArtifactRoot(target);
+        if(root && draggingPosition !== undefined)
+            draggingPosition = root.offsetTop;
+            
     }
 
     function mouseMove(event: MouseEvent) {
-        if(draggingIndex === undefined)
+        if(draggingIndex === undefined || draggingPosition === undefined)
             return;
-        draggingPosition = event.y;
+        draggingPosition += event.movementY;
     }
 
     function titleEdited(event: KeyboardEvent) {
@@ -99,6 +105,7 @@
         let index = parseInt(target.getAttribute('artifact-index')!);
         if(event.keyCode === 13) { //enter
             event.preventDefault();
+            target.blur();
             artifacts[index].title = target.textContent!;
         }
     }
@@ -110,28 +117,44 @@
         let index = parseInt(target.getAttribute('artifact-index')!);
         artifacts[index].title = target.textContent!;
     }
+
+    function traverseToArtifactRoot(target: HTMLElement | undefined) : HTMLElement | undefined {
+        while(target && !target.classList.contains('artifact'))
+            target = target.parentElement ?? undefined;
+        return target;
+    }
+
+    // let value = '';
+    // const carta = new Carta();
 </script>
 
-{#key redrawSignal}
-    <div class="artifact-list">
-        {#each artifacts as artifact, n }
-            {#if elems[n] && n === getSeperatorIndex() && draggingIndex !== undefined }
-                <div class="artifact invisible" style="height:{elems[draggingIndex].offsetHeight}px"></div>
-            {/if}
+<div style="padding: 100px;">
+    {#key redrawSignal}
+        <div class="artifact-list">
+            {#each artifacts as artifact, n }
+                {#if elems[n] && n === getSeperatorIndex() && draggingIndex !== undefined }
+                    <div class="artifact invisible" style="height:{elems[draggingIndex].offsetHeight}px"></div>
+                {/if}
 
-            <div bind:this={elems[n]} class="artifact" style="{(n === draggingIndex ? 'position:absolute;top:' + draggingPosition + 'px' : '')};user-select:{draggingIndex === undefined ? 'auto' : ''}">
-                <div class="artifact-mover-container">
-                    <div on:mousedown={mouseDown} artifact-index={n} class="artifact-mover">
-                        <img style="height:32px;user-select:none" artifact-index={n} draggable="false" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAACXBIWXMAAAsTAAALEwEAmpwYAAABjUlEQVR4nO3dUUvbUBjH4b8T/YQbiGyw45VffChsbmOwyzZSSLCIWkXNe0yeBw6UttBwfi1p04s3+fjOk/xM8iPJWfXBrF1LskkyjGub5LL6oNaq3YshSocxBp+U+V08EGPzyH275zLzJ2M7njeeeoyZY0xEmUl7wbtflI5iTER5J+0V5wVROooxEeWNtDf8xiRKRzEmonS4cc3vlJf5euByyHutzfja7PmU5G9BjGn9SXKkyJ3jcVOGovV7fFOw50uSq4IYu9f8rAQAAADwPFXXk4aVLkFSH0GQ1G+8IFlIEAAAAAAAgKWr/sNmWNkSJPURBEn9xguShQQBAAAAAABYuuo/bIaVLUFSH0GQ1G+8IFlIEAAAAIDke9HYo+txmAx7TpP8L7yetBu3ZOTRnpMk/4qDGAp2zzdj8/rTDJbsTzN6tT/NcOL+NOO7+9MMuF9GlGYydD9RmhjzaM/YaDFm1p6IIkaRiweibB65b/dcZtAOXGY5dNJnxihbMfqJshWj3nmSmyS/xtsf2i3rH1c69Aiw3QAAAABJRU5ErkJggg==">
+                <div bind:this={elems[n]} class="artifact" style="{(n === draggingIndex ? 'position:absolute;top:' + draggingPosition + 'px' : '')};user-select:{draggingIndex === undefined ? 'auto' : 'none'}">
+                    <div class="artifact-mover-container">
+                        <div on:mousedown={mouseDown} artifact-index={n} class="artifact-mover">
+                            <img style="height:32px;user-select:none" artifact-index={n} draggable="false" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAACXBIWXMAAAsTAAALEwEAmpwYAAABjUlEQVR4nO3dUUvbUBjH4b8T/YQbiGyw45VffChsbmOwyzZSSLCIWkXNe0yeBw6UttBwfi1p04s3+fjOk/xM8iPJWfXBrF1LskkyjGub5LL6oNaq3YshSocxBp+U+V08EGPzyH275zLzJ2M7njeeeoyZY0xEmUl7wbtflI5iTER5J+0V5wVROooxEeWNtDf8xiRKRzEmonS4cc3vlJf5euByyHutzfja7PmU5G9BjGn9SXKkyJ3jcVOGovV7fFOw50uSq4IYu9f8rAQAAADwPFXXk4aVLkFSH0GQ1G+8IFlIEAAAAAAAgKWr/sNmWNkSJPURBEn9xguShQQBAAAAAABYuuo/bIaVLUFSH0GQ1G+8IFlIEAAAAIDke9HYo+txmAx7TpP8L7yetBu3ZOTRnpMk/4qDGAp2zzdj8/rTDJbsTzN6tT/NcOL+NOO7+9MMuF9GlGYydD9RmhjzaM/YaDFm1p6IIkaRiweibB65b/dcZtAOXGY5dNJnxihbMfqJshWj3nmSmyS/xtsf2i3rH1c69Aiw3QAAAABJRU5ErkJggg==">
+                        </div>
+                    </div>
+                    <div>
+                        <h2 on:focusout={titleUnfocus} artifact-index={n} on:keydown={titleEdited} contenteditable="true">{artifact.title}</h2>
+                        <SvelteMarkdown source={artifact.content} />
                     </div>
                 </div>
-                <div>
-                    <h2 on:focusout={titleUnfocus} artifact-index={n} on:keydown={titleEdited} contenteditable="true">{artifact.title}</h2>
-                    <SvelteMarkdown source={artifact.content} />
-                </div>
-            </div>
-        {/each}
-    </div>
-{/key}
+            {/each}
+        </div>
+    {/key}
+</div>
 
 <svelte:window on:mouseup={mouseRelease} on:mousemove={mouseMove} />
+<!-- 
+<div style="background-color: red;">
+
+    <MarkdownEditor mode="tabs" theme="github" {carta} bind:value />
+</div> -->
